893538e039bb27ce5a33d636a0959878d0406e71
Nils Müller <75166888+nlsmllr@users.noreply.github.com>
2026-04-27 19:51:11 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -191,7 +191,7 @@ export default function CollageCreator() {
         <div className="shrink-0 flex flex-col items-center w-full">
           <Button
             onClick={downloadCollage}
-            className="rounded-xl h-12 w-full uppercase mt-4 md:mt-6"
+            className="rounded-2xl h-12 w-full uppercase mt-4 md:mt-6"
             disabled={!allImagesUploaded || isGenerating || !collageUrl}
           >Download
           </Button>
