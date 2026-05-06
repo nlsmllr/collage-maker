e3fcb2eb7eac402d73854cdfd4cb5d7af54a61ac
nlsmllr <info@nils-mueller.com>
2026-05-06 15:54:38 +0200

    PWA2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,52 @@
 import withPWAInit from "next-pwa"
 
+const runtimeCaching = [
+  {
+    urlPattern: ({ request }) => request.destination === "document",
+    handler: "NetworkFirst",
+    options: {
+      cacheName: "pages",
+      networkTimeoutSeconds: 5,
+      expiration: {
+        maxEntries: 64,
+        maxAgeSeconds: 60 * 60 * 24 * 7,
+      },
+      cacheableResponse: {
+        statuses: [0, 200],
+      },
+    },
+  },
+  {
+    urlPattern: ({ request }) =>
+      ["style", "script", "worker"].includes(request.destination),
+    handler: "StaleWhileRevalidate",
+    options: {
+      cacheName: "assets",
+      expiration: {
+        maxEntries: 128,
+        maxAgeSeconds: 60 * 60 * 24 * 30,
+      },
+      cacheableResponse: {
+        statuses: [0, 200],
+      },
+    },
+  },
+  {
+    urlPattern: ({ request }) => request.destination === "image",
+    handler: "StaleWhileRevalidate",
+    options: {
+      cacheName: "images",
+      expiration: {
+        maxEntries: 128,
+        maxAgeSeconds: 60 * 60 * 24 * 30,
+      },
+      cacheableResponse: {
+        statuses: [0, 200],
+      },
+    },
+  },
+]
+
 const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
@@ -10,6 +57,7 @@ const withPWA = withPWAInit({
   },
   cacheStartUrl: true,
   dynamicStartUrl: false,
+  runtimeCaching,
 })
 
 /** @type {import('next').NextConfig} */
